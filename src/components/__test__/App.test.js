b3dd2b750d618152c6995968739aa0a282e13eef
Kelly Davidson <kellyadavidsonuk@gmail.com>
2020-07-16 19:07:12 +0100

    initial set up

---
+++ src/components/__test__/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+
+it('shows a comment box', () => {
+  const div = document.createAttribute('div');
+    ReactDOM.render(<App />, div);
+    
+  ReactDOM.unmountComponentAtNode('div');
+});
