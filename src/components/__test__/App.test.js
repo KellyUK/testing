0722d4804cb09b1096f9e5e30b8830ad3cfb7176
Kelly Davidson <kellyadavidsonuk@gmail.com>
2020-07-16 19:30:18 +0100

    add enzyme setup

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import enzyme from 'enzyme';
 import App from '../App';
 
 it('shows a comment box', () => {
-  const div = document.createAttribute('div');
-    ReactDOM.render(<App />, div);
-    
-  ReactDOM.unmountComponentAtNode('div');
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  expect(div.innerHTML).toContain('<CommentBox/>');
+  ReactDOM.unmountComponentAtNode(div);
 });
