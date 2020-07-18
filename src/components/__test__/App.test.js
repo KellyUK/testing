6f6affb22684a1263bfc679aada508d0201f0cfd
Kelly Davidson <kellyadavidsonuk@gmail.com>
2020-07-18 13:06:03 +0100

    get enzyme to work

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import enzyme from 'enzyme';
+import Enzyme from 'enzyme';
+import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 import App from '../App';
+import CommentBox from '../CommentBox';
 
 it('shows a comment box', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  expect(div.innerHTML).toContain('<CommentBox/>');
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
 });
